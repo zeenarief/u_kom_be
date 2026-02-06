6528dbffc7526638258d6a415bab27f2770492d9
zeen-arify <zeenarify@gmail.com>
2026-02-07 06:29:42 +0700

    memperbaiki NIK supaya wajib diisi

--- internal/model/request/employee_request.go
+++ internal/model/request/employee_request.go
@@ -7,15 +7,13 @@ type EmployeeCreateRequest struct {
 	FullName         string     `json:"full_name" binding:"required"`
 	NIP              *string    `json:"nip" binding:"omitempty"`
 	JobTitle         string     `json:"job_title" binding:"required"`
-	NIK              string     `json:"nik,omitempty"` // Akan dienkripsi
+	NIK              string     `json:"nik" binding:"required"`
 	Gender           string     `json:"gender,omitempty"`
 	PhoneNumber      *string    `json:"phone_number" binding:"omitempty"`
 	Address          string     `json:"address,omitempty"`
 	DateOfBirth      *time.Time `json:"date_of_birth,omitempty"`
 	JoinDate         *time.Time `json:"join_date,omitempty"`
 	EmploymentStatus string     `json:"employment_status,omitempty"`
-	// user_id tidak disertakan di Create,
-	// karena akan di-link melalui endpoint terpisah
 }
 
 // DTO untuk Update Employee
